87a43f4a0affefb9dc4cfce941c1e571308df16c
Mahmoud khaled <164625524+Mahmoudkhaled17@users.noreply.github.com>
2026-04-05 11:02:21 +0200

    Add files via upload

---
+++ TASKS/task/mongo.js
@@ -0,0 +1,54 @@
+ const mongoose = require('mongoose')
+
+ if (process.argv.length < 3) {
+   console.log('give password as argument')
+   process.exit(1)
+ }
+
+ const password = encodeURIComponent(process.argv[2])
+
+
+// Note: Ensure the cluster URL matches your MongoDB Atlas setup
+   const url =`mongodb+srv://mahmoudkhaled1772001:${password}@cluster0.zrxpqva.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+   mongoose.set('strictQuery', false)
+
+   mongoose.connect(url)
+
+   const personSchema = new mongoose.Schema({
+     name: String,
+     number: String,
+   })
+
+
+   const Person = mongoose.model('Person', personSchema)
+
+
+
+ if (process.argv.length === 3) {
+   // List all entries
+   console.log('phonebook:')
+   Person.find({}).then(result => {
+     result.forEach(person => {
+       console.log(`${person.name} ${person.number}`)
+     })
+     mongoose.connection.close()
+   })
+ } else if (process.argv.length === 5) {
+   // Add new entry
+   const name = process.argv[3]
+   const number = process.argv[4]
+
+   const person = new Person({
+     name: name,
+     number: number,
+   })
+
+   person.save().then(result => {
+     console.log(`added ${name} number ${number} to phonebook`)
+     mongoose.connection.close()
+   })
+ } else {
+   console.log('Usage: node mongo.js <password> [<name> <number>]')
+   mongoose.connection.close()
+ }
